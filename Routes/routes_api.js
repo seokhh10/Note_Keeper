0086daceb4ab6ba8b1537542fbb8820a1f868efc
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-07-01 14:05:18 -0500

    created a POST request with a unique id-uuid, now I have an error with the server 'module cant be found'

--- Routes/routes_api.js
+++ Routes/routes_api.js
@@ -1,14 +1,23 @@
 const router = require('express').Router();
- const fs = require ('fs'); //Imports the built-in Node.js file system(fs) module. Interacting with the file system, such as reading, writing and deleting files.
+const { v4: uuidv4 } = require('uuid'); //Function generates a random UUID (universal unique identifiers). Resource: UUID npm package
+const fs = require ('fs'); //Imports the built-in Node.js file system(fs) module. Interacting with the file system, such as reading, writing and deleting files.
+
 
 //Defines the GET request to this route, '/api/notes'
 router.get('/api/notes', async (req, res) => {
     const dataBase = await JSON.parse(fs.readFileSync("db/db.json", "utf-8"));
     res.json(dataBase);
-
-    
-
 });
+
+//Defines the POST request to this route, '/api/notes'
+router.post('/api/notes', async (req, res) => {
+    const dataBase = JSON.parse(fs.readFileSync("db/bd.json", "utf-8"));
+    const newFeed = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+})
 
 
 
