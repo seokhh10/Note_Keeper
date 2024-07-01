d5f24f92e2dd6539035804ab95aa617d0f8f18f8
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-07-01 13:51:42 -0500

    created a paths for  index.html and notes.html. server still runs but the app is not working

--- Routes/routes_api.js
+++ Routes/routes_api.js
@@ -1,14 +1,14 @@
-const { route } = require('./routes_html');
-
 const router = require('express').Router();
+ const fs = require ('fs'); //Imports the built-in Node.js file system(fs) module. Interacting with the file system, such as reading, writing and deleting files.
 
-
-
-
-
+//Defines the GET request to this route, '/api/notes'
 router.get('/api/notes', async (req, res) => {
-    console.log(router);
-})
+    const dataBase = await JSON.parse(fs.readFileSync("db/db.json", "utf-8"));
+    res.json(dataBase);
+
+    
+
+});
 
 
 
--- Routes/routes_html.js
+++ Routes/routes_html.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
+const path = require('path');   //path module provides utilities for working with file and directory paths.
 
-
+//Defines the route that sends "index.html". Respond to a user when GET request is made
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
-
-
-
-
-
+//Defines the route that sends "notes.html". Respond to a user when GET request is made
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html' ))
+});
 
 
 
